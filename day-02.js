d70408628c5dafa8b181c61df988d4bb1dd7137e
Alexander Schenkel <aschenkel@kadenpartner.ch>
2018-12-02 10:25:20 +0100

    Day 2

---
+++ day-02.js
@@ -0,0 +1,337 @@
+let input = [
+  'qcsnyvpigkxmrdawlfdefotxbh',
+  'qcsnyvligkymrdawljujfotxbh',
+  'qmsnyvpigkzmrnawzjuefotxbh',
+  'qosnyvpigkzmrnawljuefouxbh',
+  'qcsnhlpigkzmrtawljuefotxbh',
+  'qcsnyvpigkzmrdapljuyfotxih',
+  'qcsnbvpiokzmrdawljuerotxbh',
+  'qcfnyvmigkzmrdawljuefotdbh',
+  'qcsnynpigkzmrdawljuefptxbp',
+  'qcsgyapigkzmrdawljuafotxbh',
+  'qcsnyvpigkzmrdapljueeotibh',
+  'qcfnyvpigkzmndawljuwfotxbh',
+  'qzsayvpigkzmrdawijuefotxbh',
+  'qcsnsvpiekzmrdawljfefotxbh',
+  'ncsnyvpigkzmrdaaljuefotxzh',
+  'qssnyvpigkzmrdawljuefotobg',
+  'qcshyipigkzmrdajljuefotxbh',
+  'qcsnyvtigkzmrdawljgeaotxbh',
+  'qcsnkvpxgkzmrdawljuefltxbh',
+  'qcsnyvpiikzmrdawljuwfoqxbh',
+  'qcsnybpigwzmqdawljuefotxbh',
+  'qcsiyvpipkzbrdawljuefotxbh',
+  'qldnyvpigkzmrdzwljuefotxbh',
+  'qcsnyvpwgkzcrdawljuefmtxbh',
+  'qcsnyvnigkzmrdahmjuefotxbh',
+  'qcsnydpigkzmrdazljuefotxnh',
+  'qcsqyvavgkzmrdawljuefotxbh',
+  'ucsnyvpigkzmrdawljuefocxwh',
+  'qcsnivpigrzmrdawljuefouxbh',
+  'tcsnyvpibkzmrdawlkuefotxbh',
+  'qcstytpigkzmrdawsjuefotxbh',
+  'qcynyvpigkzmrdawlluefotjbh',
+  'qcstyvpigkqrrdawljuefotxbh',
+  'icsnyvpizkzmrcawljuefotxbh',
+  'qcsnyvpimkzmrdavljuezotxbh',
+  'qvsnoupigkzmrdawljuefotxbh',
+  'qcsnyvpigkzmrdawwjuefftxgh',
+  'qcpnyvpijkzmrdvwljuefotxbh',
+  'qcsnyvpigkzmxdakdjuefotxbh',
+  'jcsvyvpigkqmrdawljuefotxbh',
+  'qcwnyvpigczmrsawljuefotxbh',
+  'qcsnyvpdgkzmrdawljuefoixbm',
+  'qysnyvpigkzmrdmwljuefotxbp',
+  'qcsnavpigkzmrdaxajuefotxbh',
+  'qcsfkvpigkzmrdawlcuefotxbh',
+  'qcsnyvpigkvmrdawljcefotpbh',
+  'qcsnyvpiqkkmrdawlvuefotxbh',
+  'qhsnyvpigkzmrdawnjuedotxbh',
+  'qasnlvpigkzmrdawljuefotxkh',
+  'qgsnyvpigkzmrdabpjuefotxbh',
+  'jcsnyvdigkzmrmawljuefotxbh',
+  'qcsnivpigkzmrdawljuefonxth',
+  'qcsnyjpigkzmrdawljgefotxmh',
+  'qcstyvpigkzmrdacljuefovxbh',
+  'qcsnvvpigkzmrdawljuewotrbh',
+  'qcsnyvaigdzmrdawljueuotxbh',
+  'qcsnyvpegkzmwdawljzefotxbh',
+  'qcsnevpngkzmrdawlouefotxbh',
+  'qcsnuvpigozmrdawljuefotdbh',
+  'qgsnyvpigkzmqdayljuefotxbh',
+  'qcsnyvpigkzmrdcwdjuofotxbh',
+  'qcnnyvpigkzmrzawljuefstxbh',
+  'qlsgyvpigkzmrdtwljuefotxbh',
+  'qcsnyfpigkzlroawljuefotxbh',
+  'qcsnkvwigkzmrdowljuefotxbh',
+  'qcsnrvpigkzmrdawljuvfltxbh',
+  'qcsnyvpigkzvreawljuefotxmh',
+  'qcsrgvpigkzmrdawliuefotxbh',
+  'qysnyvpigkzmrdawlxaefotxbh',
+  'qcsnyvpigizmrdlwljuefotxbi',
+  'qzsnyvpitkzmrdawljuefbtxbh',
+  'qzgnyvpigkzmrdawljuefotxih',
+  'qcsnyvpigkzmrdawlguefvtxbb',
+  'qcsnyvpigkzmidawljuefouxjh',
+  'qksnyvpigkzmrdawlruefotxhh',
+  'qcsnyvpinkzmrdaaljuefotxah',
+  'qcsnxvpigkzjrdawljuefhtxbh',
+  'qcsnyvpigkzardawlgueuotxbh',
+  'qcsnyvpiakzmrdpwljuefotxbt',
+  'qcsnyvpigkzmrdawkjuefotxgb',
+  'qcsnyvpigkzmrdawljuehocsbh',
+  'qcsnsvpigktmrdawljuefotxvh',
+  'qusnrvpigkzrrdawljuefotxbh',
+  'qcsnyhiigkzmrdawrjuefotxbh',
+  'qcsnavpigkzmrdawlfuefotxbz',
+  'qcsnyvpigkzmmdamsjuefotxbh',
+  'qcsnyvzigkzmrdcwljmefotxbh',
+  'qcsnyvpigkzmriawljuefotbbe',
+  'qcsnyvpigksmrdawljaefotxbd',
+  'qcsnyvpigkzfrdawljuefoxxmh',
+  'qcsnyvpygkrmrdawljuefotxbi',
+  'qcsngvwigfzmrdawljuefotxbh',
+  'qcsnyvpigkmkrdauljuefotxbh',
+  'qcsnyvpigxzmrdgwljuefwtxbh',
+  'qconyapigkzmrdaxljuefotxbh',
+  'qcsnydpigkzwrdawljulfotxbh',
+  'qcsnyvpimkzmmdawljuefotxch',
+  'qcsnkspigkzmrdawgjuefotxbh',
+  'qcsnyvpigkzmrdhwljfefbtxbh',
+  'qcsnyipijkztrdawljuefotxbh',
+  'qcseyvpigkrhrdawljuefotxbh',
+  'qcsnyvpivkzmrdawljuefottbb',
+  'qcsnyvpigkzmrdawlouefcjxbh',
+  'qcsnyvpigkzmrgayljuefotxbm',
+  'qcsnyvpvgkzmrdawrjujfotxbh',
+  'qcsnyvpigkzmndawljuefqtxch',
+  'qcsnyvpigbzmrdawljuefotibg',
+  'qcsnyvpigkzmseawljuefotxbv',
+  'qcsnwvpigkzmraawnjuefotxbh',
+  'mcsnyvpiqkzmrdawljuefotlbh',
+  'bcsnyvpigczmrdmwljuefotxbh',
+  'qcsnyvpigkzmrtawljuegntxbh',
+  'qcsnyvpijkzmrdawlmrefotxbh',
+  'qdsnyvpfgkzmrdawljuekotxbh',
+  'qcsnyvpigkzmrdawcjfegotxbh',
+  'qcslyvphgkrmddawljuefotxbh',
+  'qcsnyvpigkzmsdawkjuefojxbh',
+  'qzsnyvpigkzmrzawljuefmtxbh',
+  'qcsnyvpqgkzmcdawljuefttxbh',
+  'qcsnyvpbgkpmrdawljuefoqxbh',
+  'qcsnyvpigkemrdywljmefotxbh',
+  'qcsnyypigkzmrdawljmefotxwh',
+  'jcsnyvhwgkzmrdawljuefotxbh',
+  'qcsnyvpigkzmrdawljurlotxwh',
+  'qcsnnvpigzzmrdawljuefotwbh',
+  'hcsnyvpigkzmrdarljuefitxbh',
+  'qcsnyvpilkzmrfawljuefotsbh',
+  'qcsnynpigkzmldawijuefotxbh',
+  'qcsnyvpkgkjmrdawljuefotxlh',
+  'qcsnylpigkzprdawljgefotxbh',
+  'qcsnyvpigkzmrrawljnefohxbh',
+  'qcsnivpigkzmrqawlbuefotxbh',
+  'qcsgyvpigkzmrfawljuefotbbh',
+  'qccuyvpigkzmrdawyjuefotxbh',
+  'gcsnyvpigkzjrdawljuefotxby',
+  'qcsmyvpiekzbrdawljuefotxbh',
+  'qcsnyvpzgkrmrdawljuefotxbs',
+  'qesnyvpigkzmpdqwljuefotxbh',
+  'qcsnyvpigqzmrdawljuefutibh',
+  'qcdnyvpigkzirdawljfefotxbh',
+  'qcsnyvpiukzmrcrwljuefotxbh',
+  'qcsnbvpickzmrdswljuefotxbh',
+  'qcsnyvpighzmrpadljuefotxbh',
+  'qccnyvpigkzmrdawljudxotxbh',
+  'qcsnyvpigkzmrdabljuesotxlh',
+  'qcsnyvpigkzmrrawlruefozxbh',
+  'qconyzpigkzmrdawljuefotjbh',
+  'qclnyvpigkzmrdxwljuefotbbh',
+  'qcsnygpigkzmrdawlhuefooxbh',
+  'qcsnyvpigkzmvdawljuefntxnh',
+  'qcskyvpigkzmreawljuefotubh',
+  'qrsnyvpxgkzmrdawljuefotxbz',
+  'qclnyvpigtamrdawljuefotxbh',
+  'qcsnyvpigkzmrdawojxefoyxbh',
+  'qcsnyvpinkzmrdakljuwfotxbh',
+  'qcsnyvpiykzmedawljuefgtxbh',
+  'qcsayvpigkcmrdawijuefotxbh',
+  'qcsnyvuiekzmrdamljuefotxbh',
+  'qcdnyvpigkzmrdawnjuefoxxbh',
+  'qcsnfvpwgszmrdawljuefotxbh',
+  'qcsnycpigkzmrdawljqefotxih',
+  'qcslyvphgkrmrdawljuefotxbh',
+  'ecsnyvpigkzmrdawykuefotxbh',
+  'qcsayvpigkzmraawljuekotxbh',
+  'qcsnyvpigkdmrdawljuewofxbh',
+  'qcznyvpigkzqrdawljuefotxnh',
+  'qcsnyvplgkzmrdawljiefotlbh',
+  'qcsnyvpigkzmroewljuefotbbh',
+  'qcvnyvpigkzvrdawujuefotxbh',
+  'qcanyypigkzmrdaeljuefotxbh',
+  'qcsnyvwigkzmrdewljuefotxqh',
+  'qcsryvpigkvmrdawljuefotabh',
+  'pcsnyvpigkwmrdawljueforxbh',
+  'qcsncvpigkzmrdawljuefotwmh',
+  'qcsnyvpigozmrdawljudfozxbh',
+  'qcsnynpigkzmrbawhjuefotxbh',
+  'qcsnyvuigkzmrqawljuefotxch',
+  'qcsnyvpickzmrdawljueeofxbh',
+  'qcsnyvpigkzgrdawljueiouxbh',
+  'qcsnyvpigkztrdawljuxnotxbh',
+  'qcsnyvpigwzvrdawljfefotxbh',
+  'qcsnyvpilkzmrdawljuefotxcz',
+  'qcsnjvpigkzmrdawljuefoywbh',
+  'qhsnyvpigyzmrdawljuhfotxbh',
+  'qcsnyvpirkzmfdawljuffotxbh',
+  'qcsjyvpigkzmvdawljuefotxzh',
+  'qcszivpirkzmrdawljuefotxbh',
+  'qwsnyvpigkzmtdawljuefetxbh',
+  'qcrntvpigkzordawljuefotxbh',
+  'qrsnyvpigkzmsdawljrefotxbh',
+  'qcsnyviivkzmrdazljuefotxbh',
+  'ecsnyvpigkzmrdawyjuefotxbw',
+  'qnsnyvpkgkzmrdawljueqotxbh',
+  'qcsyyppigkzmrdawljuefotxba',
+  'qcsnyvpigkzhrdpwljuefouxbh',
+  'ucsnyvpigkzmrdawojuefouxbh',
+  'qysnyvpigkzmrdawljukfotxbd',
+  'qcjnyvpigkzmrdalljfefotxbh',
+  'fcsnyapigkmmrdawljuefotxbh',
+  'qcnnkvpigkzmrdawljuefctxbh',
+  'ocsnyvpigkzmsdawljuefotxbl',
+  'qcsnyvpiakomrdawpjuefotxbh',
+  'qcsnyvpigkzmrdawljvefbtxwh',
+  'qcsnuvpigkzmvdfwljuefotxbh',
+  'qcsnyapihkzmrdagljuefotxbh',
+  'qzsnyvpigkzmrdawtjuefotxgh',
+  'qcsnyvpigkzmrdawljuefomyah',
+  'ocsnyvpigkzqrdawljuefotxbt',
+  'qnsnyvpigkzmrdawljvevotxbh',
+  'icsnyvpigkzmrdawljuefntxbt',
+  'qcsnyvpigkzdrdawljuefotbbm',
+  'scsnyvpigkzmrgawljuofotxbh',
+  'qcsnydpigkzmrdowljuefotkbh',
+  'qcsnyvtikkzmrdawljuefolxbh',
+  'qcsiyvpigkcmrddwljuefotxbh',
+  'qyrnyvpigkzmodawljuefotxbh',
+  'pcsndvpfgkzmrdawljuefotxbh',
+  'qcsnyvkigkhmriawljuefotxbh',
+  'qcsnyvpigkzmsdmwlkuefotxbh',
+  'dosnyvpigkzmrdawdjuefotxbh',
+  'qcnnnvpigkzmrdzwljuefotxbh',
+  'qcsnyvpivkumrdailjuefotxbh',
+  'qcsnyvpigkzmrdswljuzfotxbz',
+  'qcscynpigkzmrdawljuefotxbc',
+  'qeanyvpigkzmrdawijuefotxbh',
+  'qclnylpigkzmrdawljuefotxyh',
+  'qcsnyvpigkzmrdawljbefowxbp',
+  'qcsnyvpagkzmrdawljuefolebh',
+  'qxsiyvpigkzmrdawljuefotxgh',
+  'qcsnyvpigkynrdawljuefoqxbh',
+  'qcsnevpigkzmrdxwgjuefotxbh',
+  'qcsnyvpdgkzlrdawljeefotxbh',
+  'qcsnyvpigkzmrgawljxbfotxbh',
+  'ecsnyvpigkzmrdbwbjuefotxbh',
+  'qcsnyvpigkzmraawujuefocxbh',
+  'qcsnyvpihkzmrdawljuefouxbn',
+  'fgsqyvpigkzmrdawljuefotxbh',
+  'qcsnyvpigkmmrdawajuefotnbh',
+  'qcsnyvvigkzmrdahljudfotxbh',
+  'qcsnyvpixkzmrdqwljutfotxbh',
+  'ncsnyvpickzmrdawljuehotxbh',
+  'qcsnyvpizkzmrdawlpuefotxbp',
+  'wcsnyvfigkzmrdakljuefotxbh',
+  'qcsnyvpigkznrdhwljupfotxbh',
+  'jcsnyvpigkpmzdawljuefotxbh',
+  'qcsnyppigkkmrdawljujfotxbh',
+  'qcsnyvpigkumrdaeljuefodxbh',
+  'qcsnyvhigkzmrdrwljuefodxbh',
+  'qcsnyvpigkacrdawtjuefotxbh',
+  'qcsnyvpigkzmylawlquefotxbh',
+];
+
+class BoxInfo {
+  constructor(id) {
+    this.id = id;
+    this.has2Letter = false;
+    this.has3Letter = false;
+    this.examine();
+  }
+
+  examine() {
+    for (let i = 0; i < this.id.length; i++) {
+      let nr = this.countChar(this.id[i]);
+      if (nr === 2) {
+        this.has2Letter = true;
+      }
+      if (nr === 3) {
+        this.has3Letter = true;
+      }
+      if (this.has2Letter && this.has3Letter) {
+        break;
+      }
+    }
+  }
+
+  countChar(char) {
+    let count = 0;
+    for (let i = 0; i < this.id.length; i++) {
+      if (char === this.id[i]) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+    countMatchChars(otherBox) {
+        let count = 0;
+        for (let i = 0; i < this.id.length; i++) {
+            if (this.id[i] !== otherBox.id[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+let all = input
+  .map(id => {
+    return new BoxInfo(id);
+  });
+let output = all.filter(info => info.has2Letter || info.has3Letter);
+
+let count2 = 0,
+    count3 = 0;
+output.forEach(info => {
+    if (info.has2Letter) {
+        count2++;
+    }
+    if (info.has3Letter) {
+        count3++;
+    }
+});
+console.log(`Boxes with 2 same letters: ${count2}, Boxes with 3 same letters: ${count3}, checksum( Solution Day 2 Part 1 ): ${count2*count3}`);
+
+// 2nd part: Find boxes which differs only in 1 character in the string
+let foundBoxes = [];
+for (let iA = 0; iA < all.length-1; iA++) {
+    for (let iB = iA+1; iB < all.length; iB++) {
+        if (all[iA].countMatchChars(all[iB]) === 1) {
+            foundBoxes.push(all[iA]);
+            foundBoxes.push(all[iB]);
+        }
+    }
+}
+
+console.log("Matching boxes",foundBoxes);
+
+// Ouptut matching chars:
+let match = "";
+for (let i = 0; i < foundBoxes[0].id.length; i++) {
+    if (foundBoxes[0].id[i] === foundBoxes[1].id[i]) {
+        match += foundBoxes[0].id[i];
+    }
+}
+
+console.log(`Matching chars (Solution Day 2 Part 2): ${match}`);
