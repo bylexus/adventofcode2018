2c33e6dd2b3c0ff7954e99a9cd10cdf6783065e0
Alexander Schenkel <aschenkel@kadenpartner.ch>
2018-12-18 11:05:01 +0100

    Day 17: Work in progress. It works, but not correctly. And for the 2nd part, I need the flow (|) info, too. So, again, another solution must be found...

---
+++ day-17.js
@@ -0,0 +1,233 @@
+// ----- Use with node --harmony (for Object.values)
+
+let matcher = new RegExp('(x|y)=(\\d+)\\,\\s*(x|y)=(\\d+)\\.\\.(\\d+)');
+let fs = require('fs');
+let maxX = 0,
+    maxY = 0,
+    minX = Infinity,
+    minY = Infinity;
+let input = fs
+    // .readFileSync('./day-17-input-small.txt', { encoding: 'UTF-8' })
+    .readFileSync('./day-17-input.txt', { encoding: 'UTF-8' })
+    .trim()
+    .split('\n')
+    .map(line => {
+        let match = line.match(matcher);
+        if (match) {
+            let [full, fixAxis, at, axis, from, to] = match;
+            at = Number(at);
+            from = Number(from);
+            to = Number(to);
+            if (axis === 'x') {
+                maxX = Math.max(maxX, from);
+                maxX = Math.max(maxX, to);
+                minX = Math.min(minX, from);
+                minX = Math.min(minX, to);
+                maxY = Math.max(maxY, at);
+                minY = Math.min(minY, at);
+            }
+            if (axis === 'y') {
+                maxY = Math.max(maxY, from);
+                maxY = Math.max(maxY, to);
+                minY = Math.min(minY, from);
+                minY = Math.min(minY, to);
+                maxX = Math.max(maxX, at);
+                minX = Math.min(minX, at);
+            }
+            return {
+                axis,
+                at,
+                from,
+                to
+            };
+        }
+    });
+
+let earth = new Array(maxY + 1);
+console.log(minX, maxX, minY, maxY);
+
+init();
+// print();
+dripFromSource([0, 500]);
+print();
+let waterCount = countWater();
+console.log(`Day 17: Water count (Solution 1): ${waterCount}`);
+
+function init() {
+    // init empty ground
+    for (let y = 0; y < earth.length; y++) {
+        earth[y] = new Array(maxX + 2);
+        for (let x = 0; x < earth[y].length; x++) {
+            earth[y][x] = '.'; //sand
+        }
+    }
+    // fill in clay:
+    input.forEach(line => {
+        for (let i = line.from; i <= line.to; i++) {
+            if (line.axis === 'x') {
+                earth[line.at][i] = '#';
+            }
+            if (line.axis === 'y') {
+                earth[i][line.at] = '#';
+            }
+        }
+    });
+
+    // fill in source:
+    earth[0][500] = '+';
+}
+
+/**
+ * drips water from the given source coordinate down, until no more water can run down.
+ *
+ * Steps:
+ * 1. fill downwards until we reach either ground or the bottom line
+ * 2. then fill to the left and right of the bottom, if landed on ground,
+ *    checking each side for a new source (a border where water can fall down).
+ *    If this is the case, this drip can stop, after processing the new source(s) recursively.
+ * 3. recursively process new sources
+ *
+ * @param {array} sourceCoord
+ */
+function dripFromSource(sourceCoord) {
+    let actCoord = [sourceCoord[0] + 1, sourceCoord[1]];
+    // step 1: drip down water:
+    while (earth[actCoord[0]] && (earth[actCoord[0]][actCoord[1]] === '.' || earth[actCoord[0]][actCoord[1]] === '~')) {
+        let [y, x] = actCoord;
+        earth[y][x] = '~';
+        if (y + 1 >= earth.length) {
+            break;
+        }
+        actCoord = [y + 1, x];
+    }
+
+    // we reach ground, check what we reached:
+    if (isBottom(actCoord)) {
+        // console.log('bottom reached at', actCoord);
+        return actCoord;
+    }
+    if (isClay(actCoord)) {
+        // console.log('clay reached at', actCoord);
+        while (!coordsMatch(actCoord, sourceCoord)) {
+            let continueFill = true;
+            actCoord = [actCoord[0] - 1, actCoord[1]];
+            let sourceLEndCoords = null;
+            let sourceREndCoords = null;
+
+            let sourceL = fillLeft(actCoord);
+            if (sourceL === -1) {
+                // -1 means: we have reached an existing source, skip
+                continueFill = false;
+            } else if (sourceL) {
+                sourceLEndCoords = dripFromSource(sourceL);
+                if (!coordsMatch(sourceLEndCoords, sourceL)) {
+                    continueFill = false;
+                }
+            }
+            let sourceR = fillRight(actCoord);
+            if (sourceR === -1) {
+                // -1 means: we have reached an existing source, skip
+                continueFill = false;
+            } else if (sourceR) {
+                sourceREndCoords = dripFromSource(sourceR);
+                if (!coordsMatch(sourceREndCoords, sourceR)) {
+                    continueFill = false;
+                }
+            }
+            // if (!fakeSource && (sourceL || sourceR)) {
+            if (!continueFill) {
+                // end here, as sources do the rest:
+                return false;
+            }
+        }
+    }
+    return actCoord;
+}
+
+function coordsMatch(coord1, coord2) {
+    return coord1[0] === coord2[0] && coord1[1] === coord2[1];
+}
+
+function fillLeft(coord) {
+    return fillX(coord, -1);
+}
+
+function fillRight(coord) {
+    return fillX(coord, 1);
+}
+
+function fillX(coord, dir) {
+    // fills to the left (dir = -1) or right (dir=1) of the given coordinate, until we reach clay or a border.
+    // If we reach a border, we return that coordinate as a new source's location.
+    if (coord[1] === 0 || coord[1] >= earth[0].length) {
+        return null;
+    }
+    let actCoord = [coord[0], coord[1] + dir];
+    while (actCoord[1] >= 0 && actCoord[1] < earth[0].length) {
+        // fill sand with water:
+        if (earth[actCoord[0]][actCoord[1]] === '.') {
+            earth[actCoord[0]][actCoord[1]] = '~';
+        }
+        // have we reached clay? return.
+        if (earth[actCoord[0]][actCoord[1]] === '#') {
+            return null;
+        }
+        // have we reached an existing source? return.
+        if (earth[actCoord[0]][actCoord[1]] === '+') {
+            // special case: If BEHIND the existing source is water, we ignore this source: Then it is part of a
+            // basin:
+            if (earth[actCoord[0]][actCoord[1]+dir] === '~') {
+                earth[actCoord[0]][actCoord[1]] = '~';
+                return null;
+            }
+            // return actCoord;
+            return -1;
+        }
+        // have we reached a border? OK, then this is a new source and the end
+        // of fill:
+        if (earth[actCoord[0] + 1][actCoord[1]] === '.') {
+            earth[actCoord[0]][actCoord[1]] = '+';
+            return actCoord;
+        }
+        actCoord = [actCoord[0], actCoord[1] + dir];
+    }
+    return null;
+}
+
+function isClay(coord) {
+    let [y, x] = coord;
+    if (x < 0 || x >= earth[0].length) {
+        return false;
+    }
+    if (y < 0 || y >= earth.length) {
+        return false;
+    }
+    return earth[y][x] === '#';
+}
+
+function isBottom(coord) {
+    return coord[0] >= earth.length - 1 && earth[coord[0]][coord[1]] !== '#';
+}
+
+function countWater() {
+    let count = 0;
+    for (let y = minY; y <= maxY; y++) {
+        for (let x = 0; x < earth[y].length; x++) {
+            if (earth[y][x] === '~' || earth[y][x] === '+') {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+function print() {
+    let str = '';
+    for (let y = 0; y < earth.length; y++) {
+        for (let x = minX - 1; x <= maxX + 1; x++) {
+            str += earth[y][x];
+        }
+        str += '\n';
+    }
+    console.log(str);
+}
